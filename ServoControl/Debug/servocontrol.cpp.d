3e681823e44c7b04c8db09c2b37997615844acb4
TOSHIBA-TC\Theuns <toensbotes@gmail.com>
2017-11-12 13:01:49 +0200

    PID controll working, but with some timing issues

---
+++ ServoControl/Debug/servocontrol.cpp.d
@@ -0,0 +1,14 @@
+C:\Users\Theuns\AppData\Local\Temp\VMBuilds\servocontrol\nano_atmega328\Debug\servocontrol.cpp.o: \
+ C:\Users\Theuns\AppData\Local\Temp\VMBuilds\servocontrol\nano_atmega328\Debug\servocontrol.cpp \
+ C:\Users\Theuns\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.20\cores\arduino/Arduino.h \
+ C:\Users\Theuns\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.20\cores\arduino/binary.h \
+ C:\Users\Theuns\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.20\cores\arduino/WCharacter.h \
+ C:\Users\Theuns\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.20\cores\arduino/WString.h \
+ C:\Users\Theuns\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.20\cores\arduino/HardwareSerial.h \
+ C:\Users\Theuns\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.20\cores\arduino/Stream.h \
+ C:\Users\Theuns\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.20\cores\arduino/Print.h \
+ C:\Users\Theuns\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.20\cores\arduino/Printable.h \
+ C:\Users\Theuns\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.20\cores\arduino/USBAPI.h \
+ C:\Users\Theuns\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.20\cores\arduino/Arduino.h \
+ C:\Users\Theuns\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.20\variants\eightanaloginputs/pins_arduino.h \
+ C:\Users\Theuns\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.20\variants\eightanaloginputs/../standard/pins_arduino.h
